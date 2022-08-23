f682ead78725026dd6e049933f2261237535885f
Ashith V L <ashithvl@gmail.com>
2022-08-23 21:49:04 +0530

    1._Basic_Branch

--- src/main/java/com/codeWithAshith/SpringSecurity/SpringSecurityApplication.java
+++ src/main/java/com/codeWithAshith/SpringSecurity/SpringSecurityApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityApplication {
 
+	// enter localhost:8080/ in chrome or postman
 	// username - user
 	// password - from console
+
+
+
+	// cookie will be sored in -> inspect -> application -> Cookies
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
